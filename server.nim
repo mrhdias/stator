aee02e7f1d355af865b912a70b7eff92e759ec5b
Henrique Dias <mrhdias@gmail.com>
2019-03-02 23:23:17 +0000

    Fixed a bug in Json's response

--- server.nim
+++ server.nim
@@ -155,7 +155,7 @@ $1
       var msg: JsonNode
       if httpbody.data.len > 0:
         let jsonNode = parseJson(httpbody.data)
-        msg = %* {"login": jsonNode["login"], "password": $jsonNode["password"], "status": "ok" }
+        msg = %* {"login": jsonNode["login"], "password": jsonNode["password"], "status": "ok" }
       else:
         msg = %* {"status": "not ok" }
 
