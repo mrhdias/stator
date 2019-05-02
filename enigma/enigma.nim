80f34959693776b5a6c84ef79b0489cacc6670c7
Henrique Dias <mrhdias@gmail.com>
2019-05-02 21:41:58 +0100

    Fix the package incorrect structure.

---
+++ enigma/enigma.nim
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2019 Henrique Dias
+# MIT License - Look at LICENSE for details.
+#
+import routes
+import asynchttpserver
+import asynchttpbodyparser
+import asynchttpserver
+
+export routes
+export asynchttpserver
+export asynchttpbodyparser
+export asynchttpserver
