86d17e95b0818ab40d3bf433b1daf0d1e72efb98
Henrique Dias <mrhdias@gmail.com>
2019-05-15 15:57:10 +0100

    Added basic initializer

--- stator.nim
+++ stator.nim
@@ -2,10 +2,39 @@
 # Copyright (C) 2019 Henrique Dias
 # MIT License - Look at LICENSE for details.
 #
+import macros
+import asyncdispatch
+from strutils import `%`
 import stator/[routes, asynchttpserver, asynchttpbodyparser, asynchttpserver, basicauth]
 
+export asyncdispatch
 export routes
 export asynchttpserver
 export asynchttpbodyparser
 export asynchttpserver
 export basicauth
+
+type
+  Settings = object
+    port: int
+    maxBody: int
+
+proc listenFor*(port: int = 8080, maxBody: int = 8388608): Settings =
+  return Settings(port: port, maxBody: maxBody)
+
+macro withStator*(args, body: untyped): untyped =
+  if args.kind != nnkInfix or args.len != 3 or not eqIdent(args[0], "->"):
+    error "'(value) -> (name)' expected in withStator, found: '$1'" % [args.repr]
+
+  let varValue = args[1]
+  let varName = args[2]
+
+  template withStatorImpl(name, value, body) =
+    let settings = value
+    proc handler(name: Request) {.async gcsafe.} =
+      body
+    let server = newAsyncHttpServer(maxBody=settings.maxBody)
+    waitFor server.serve(Port(settings.port), handler)
+
+  getAst(withStatorImpl(varName, varValue, body))
+  
