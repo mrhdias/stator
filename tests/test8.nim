b3239b2037369c6ab624b24c1853f7fb32c6fffb
Henrique Dias <mrhdias@gmail.com>
2019-05-15 20:50:52 +0100

    Added another test

---
+++ tests/test8.nim
@@ -0,0 +1,60 @@
+import stator
+from strutils import `%`
+
+proc home(req: Request) {.async.} =
+  let htmlpage = """
+<!Doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+Home Sweet Home...
+<p>
+Test Routes:
+<ul>
+<li><a href="method">Methods</a></li>
+<li><a href="users/test">Regex Capture</a></li>
+<li><a href="unknow">Unknow</a></li>
+</ul>
+</p>
+</body>
+</html>
+"""
+
+  await req.respond(Http200, htmlpage)
+
+
+proc whichMethod(req: Request) {.async.} =
+  await req.respond(Http200, "Method: $1" % $req.reqMethod)
+
+
+proc testRegex(req: Request, user: string) {.async.} =
+  if user == "test":
+    await req.respond(Http200, "Result: \"$1\" user exist" % user)
+  else:
+    await req.respond(Http200, "Result: \"$1\" user not exist" % user)
+
+
+proc main() =
+
+  withStator listenFor(port=8080, maxBody=10240) -> req:
+
+    routes:
+
+      via("/", @["GET"]):
+        await req.home()
+
+      via("/method", @["GET", "POST"]):
+        await req.whichMethod()
+
+      via(r"/users/(\w+)".re, @["GET"]):
+        await req.testRegex(captures[0])
+
+      via(r".+/([a-z]+\.[a-z]+)$".re, @["GET"]):
+        await req.respond(Http404, "File \"$1\" not found!" % captures[0])
+
+      # if unknow route
+      await req.fileserver()
+
+main()
